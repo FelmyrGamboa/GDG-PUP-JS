143760e59bbbf40a3484ff8fa2219c5f2f794805
FelmyrGamboa <felmyrgamboa23@gmail.com>
2025-02-22 21:46:03 +0800

    Finished last js exercise

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,21 @@
 // your code starts here
+
+const buttonMinus = document.querySelector("#button-subtract");
+const buttonAdd = document.querySelector("#button-add");
+const counter = document.querySelector("#counter-text");
+
+buttonAdd.addEventListener('click', function(){
+    let new_count = parseInt(counter.innerHTML) + 1;
+    console.log(new_count);
+    counter.innerHTML = new_count;
+})
+
+buttonMinus.addEventListener('click', function(){
+    if (parseInt(counter.innerHTML) === 0){
+        pass
+    }
+    else{
+        new_count = parseInt(counter.innerHTML) - 1;
+        counter.innerHTML = new_count;
+    }
+})
